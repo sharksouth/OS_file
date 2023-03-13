744dd138bd953b102da4a7717e8ed986c5c49b23
sharksouth <shark_south@live.cn>
2023-03-13 17:14:54 +0800

    3.13 finished without test

---
+++ delete.h
@@ -0,0 +1,16 @@
+//删除
+#include <stdio.h>
+#include "structure.h"
+
+delete(char *filename)
+{
+	unsigned int dinodeid;
+	struct inode *inode;
+
+	dinodeid=namei(filename);
+	if(dinodeid!=(int)NULL)
+		inode=iget(dinodeid);
+	inode->di_number--;
+	iput(inode);
+}
+
--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "structure.h"
-#include "creat.h"
+#include "create.h"
 #include "access.h"
 #include "ballfre.h"
 #include "close.h"
@@ -33,13 +33,13 @@ unsigned short usr_id;
 char usr_p[12];
 char sel;
 char temp_dir[12];
-main()
+int main()
 {
-    unigned short ab_fd1,ab_fd2,ab_fd3,ab_fd4,i,j;
+    unigned short ab_fd1, ab_fd2, ab_fd3, ab_fd4, i, j;
     char *buf;
     int done = 1;
     printf("\nDo you want to format the disk(y or n)?\n");
-    if(getchar()=='y')
+    if (getchar() == 'y')
     {
         printf("\nFormat will erase all context on the disk \n");
         printf("Formating...\n");
@@ -47,15 +47,54 @@ main()
         printf("\nNow will install the fillsystem,please wait...\n");
         install();
         printf("\n----Login----\nPlease input your userid:");
-        scanf("%u",&usr_id);
+        scanf("%u", &usr_id);
         printf("\nPlease input your password:");
-        scnaf("%s",&usr_p);
-        if(!login(usr_id,usr_p))
-        return;
+        scnaf("%s", &usr_p);
 
-       
-        
+        if (!login(usr_id, usr_p))
+            return;
+
+        /*login(2118,"abcd");*/
+        while (done)
+        {
+            printf("\n Please Select Your Operating\n");
+            printf("-1----ls\n-2----mkdir\n-3----change dir\n-4----create file\n-0----Logout\n");
+            sel = getche();
+            switch (sel)
+            {
+            case '1': // 显示目录内容
+                _ls();
+                break;
+            case '2': // 创建目录
+                printf("please iunput dir name");
+                scanf("%s", temp_dir);
+                chdir(temp_dir);
+                break;
+            case '3': // 改变当前目录
+                printf("please input file name");
+                scanf("%s", temp_dir);
+                chdir(temp_dir);
+                ab_fd1 = creat(2178, temp_dir, 01777);
+                buf = (char *)malloc(BLOCKSIZ * 6 + 5)
+            case '4': // 创建文件
+                printf("please input file name");
+                scanf("%s",temp_dir);
+                ab_fd1 = creat(2118,temp_dir,01777);
+                buf = (char*)malloc(BLOCKSIZ*6+5);
+                write(ab_fd1,buf,BLOCKSIZ *6 +5);
+                close(0,ab_fd1);
+                free(buf);
+                break;
+            case '0':   //退出
+                logout(usr_id);
+                halt();
+                done = 0;
+            default:
+                printf("error!\nNo such command,please try again.\nOr you can sak your teacher for help.\n");
+                break;
+            }
+        }
     }
+    else
+        printf("User ancseled\nGood Bye\n");
 }
-
-
